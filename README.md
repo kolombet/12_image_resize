5c021fafe6c9a98cb496bee411c6577a49161dfa
anonra <anonrab@ya.ru>
2018-03-25 17:07:31 +0000

    inital commit (by error, worked at original repository, without fork :()

--- README.md
+++ README.md
@@ -1,7 +1,46 @@
 # Image Resizer
 
-[TODO. There will be project description]
+This script is small *Pillow* library wrapper, to simplify image resizing. 
+You can use parameters to resize image:
+1. **-W, --width**
+Image result width. Other side will be scaled, to save image proportions, if height argument not specified
+2. **-H, --height**
+Image result height. Other side will be scaled, ot save image proportions, if width argument not specified
+3. **-S, --scale**
+Image result scale. Using this argument, it will ignore width and height parameters
+4. **-I, --input**  
+Input image for resize. Must be jpg or png image format
+5. **-O, --output** 
+Where save result image. If not specified, script will save result with format: [*name of original file*]_[*width*]x[*height*]
+
+# Quickstart
+
+To launch this script you need python3 installed
+Use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
+
+```bash
+pip install -r requirements.txt # alternatively try pip3
+```
+
+Example of script launch on Linux, __Python 3.5__:
+
+```bash
+$ python3 image_resize.py -I image.png -W 100
+resized image saved as image_100x105.png
+$ python3 image_resize.py -I image.png -S .3 -O thumnail.png
+resized image saved as thumnail.png
+$ python3 image_resize.py -I image.png -W 100 -H 100 -O rectangle.png
+resized image saved as rectangle.png
+```
 
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
+
+
+# Anti-Duplicator
+
+This script searches directory _default_ in current working directory and find file duplicates in it
+Two files are considered duplicate, if they have same name and same size. Content may be different.
+
+You can specify input directory with parameter __-p__ or __--path__
--- image_resize.py
+++ image_resize.py
@@ -1,8 +1,107 @@
+from PIL import Image
+from pathlib import Path
+import argparse
+import sys
 
 
-def resize_image(path_to_original, path_to_result):
-    pass
+def calculate_size_by_width(target_width, width, height):
+    target_height = height * target_width / width
+    return target_width, target_height
 
 
-if __name__ == '__main__':
-    pass
+def calculate_size_by_height(target_height, width, height):
+    target_width = width * target_height / height
+    return target_width, target_height
+
+
+def calculate_size_by_scale(scale, width, height):
+    return width*scale, height*scale
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    width_help = """Image result width. Other side will be scaled, to save
+image proportions, if height argument not specified"""
+    height_help = """Image result height. Other side will be scaled, ot save
+image proportions, if width argument not specified"""
+    scale_help = """Image result scale. Using this argument, it will
+ignore width and height parameters"""
+    input_help = "Input image for resize. Must be jpg or png image format"
+    output_help = """Where save result image. If not specified, it will
+            save it in format: [name of original file]_[width]x[height]"""
+    parser.add_argument(
+        "-W",
+        "--width",
+        dest="width",
+        help=width_help
+    )
+    parser.add_argument(
+        "-H",
+        "--height",
+        dest="height",
+        help=height_help
+    )
+    parser.add_argument(
+        "-S",
+        "--scale",
+        dest="scale",
+        help=scale_help
+    )
+    parser.add_argument(
+        "-I"
+        "--input",
+        dest="input_file",
+        help=input_help
+    )
+    parser.add_argument(
+        "-O",
+        "--output",
+        dest="output",
+        help=output_help
+    )
+    return parser.parse_args()
+
+
+def resize(input_path, params, output_path):
+    image = Image.open(input_path)
+    if params.scale:
+        size = calculate_size_by_scale(float(params.scale), *image.size)
+    elif params.width and not params.height:
+        size = calculate_size_by_width(float(params.width), *image.size)
+    elif params.height and not params.width:
+        size = calculate_size_by_height(float(params.height), *image.size)
+    elif params.width and params.height:
+        size = float(params.width), float(params.height)
+    else:
+        return None
+    size = tuple([round(x) for x in size])
+
+    image_path = Path(params.input_file)
+    suffix = image_path.suffix[1:]
+
+    if not output_path:
+        output_path = "{}_{}x{}.{}".format(
+            image_path.stem,
+            size[0],
+            size[1],
+            suffix
+        )
+    image.thumbnail(size, Image.ANTIALIAS)
+    image.save(output_path, suffix)
+    return output_path
+
+
+if __name__ == "__main__":
+    args = get_args()
+    if not args.input_file:
+        sys.exit("input image required")
+    if not args.scale and not args.width and not args.height:
+        sys.exit("please specify any resize parameters")
+    if args.scale:
+        if args.width or args.height:
+            sys.exit("if scale specified, width and height forbidden")
+    output_path = resize(args.input_file, args, args.output)
+    if output_path:
+        print("resized image saved as {}".format(output_path))
+    else:
+        print("error: image not resized")
